30dfbf2b79051ee81a14606994a6ef617f8d06fd
abhi11082005 <133665699+abhi11082005@users.noreply.github.com>
2025-03-26 12:56:41 +0530

    Update auth.js

--- service/auth.js
+++ service/auth.js
@@ -6,8 +6,11 @@ function setUser(sessionId,user) {
     return data.keys()
 }
 function getUser(sessionId) {
-    console.log(sessionIdToUserMap.get(sessionId))
+    if (!sessionIdToUserMap.has(sessionId)) {
+        console.log("Session ID not found:", sessionId);
+        return null;
+    }
+    console.log("User found:", sessionIdToUserMap.get(sessionId));
     return sessionIdToUserMap.get(sessionId);
-
 }
 export {setUser,getUser}
